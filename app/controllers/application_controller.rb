00838843b6c7cd0eb622a52ba524efb8273ba721
Leonardo Sarra <leonardosarra@outlook.com>
2018-06-29 16:06:59 +0200

    Authorization: Experimenting with CanCanCan authorization management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
         request.env['omniauth.origin'] || root_path
     end
+
+    rescue_from CanCan::AccessDenied do |exception|
+        redirect_to root_url, alert: exception.message
+    end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_book, only: [:show, :edit, :update, :destroy]
-
+  load_and_authorize_resource
   # GET /books
   # GET /books.json
   def index
