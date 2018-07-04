ff1d3c189d08a6fcd9d9c34e780079542e8d32c8
Leonardo Sarra <leonardosarra@outlook.com>
2018-07-04 12:46:16 +0200

    Login: Replace old devise's login page and add login controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get '/book_api', to: 'api#get_info'
+  devise_scope :user do
+    get '/users/login_email', to: 'login#login_email'
+  end
 end
