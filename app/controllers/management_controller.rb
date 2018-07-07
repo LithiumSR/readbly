c85b99d40bccca1909f56becbadb6442d438141d
Leonardo Sarra <leonardosarra@outlook.com>
2018-07-07 20:04:09 +0200

    User: Add role list

---
+++ app/controllers/management_controller.rb
@@ -0,0 +1,56 @@
+class ManagementController < ApplicationController
+  #before_action :isEnabled
+  def manage_users
+
+  end
+
+  def change_role
+    email = params[:id]
+    newrole = params[:changes][:role]
+    if !User::ROLES.include?(newrole)
+      redirect_to 'manage_users' and return
+    end
+    user = User.find_by(email: email)
+    for role in User::ROLES
+      user.remove_role(role.downcase)
+    end
+    user.add_role(newrole.downcase)
+    redirect_to '/manage_users' and return
+  end
+
+  def promote
+    email = params[:id]
+    role = params[:role]
+    if !User::ROLES.include?(role)
+      redirect_to 'manage_users' and return
+    end
+    user = User.find_by(email: email)
+    if role=='Operator'
+      user.add_role(:operator)
+    elsif role=='Admin'
+      user.add_role(:admin)
+    end
+    redirect_to '/manage_users' and return
+  end
+
+  def demote
+    email = params[:id]
+    role = params[:role]
+    if !User::ROLES.include?(role)
+      redirect_to 'manage_users' and return
+    end
+    user = User.find_by(email: email)
+    if role=='Operator'
+      user.remove_role(:operator)
+    elsif role=='Admin'
+      user.remove_role(:admin)
+    end
+    redirect_to '/manage_users' and return
+  end
+
+  def isEnabled
+    if !current_user.has_role? :admin
+      redirect_to root_path alert: "User not enabled to manage users"
+    end
+  end
+end
