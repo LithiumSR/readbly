181cc44e74e981f1e0a23bfd5ee87603e9cf815e
Leonardo Sarra <leonardosarra@outlook.com>
2018-07-07 21:04:15 +0200

    Auth: Update CanCanCan's Abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,18 @@ class Ability
     user ||= User.new
     #if user.client?
     #  can :manage, Product, user_id: user.id
-    if user.admin?
-      can :manage
-    else
+    if user.has_role? :admin
+      can :manage, :all
       can :read, :all
+    elsif (user.has_role? :operator)
+      can :read, Book
+      can :manage, Book
+      #can :manage, Reservation
+      #can :read, Reservation
+    elsif(user.has_role? :user)
+      can :read,Book
+      #can :manage, Reservation, user_id: user.username
+      #can :read, Reservation
     end
   end
 end
