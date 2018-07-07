d0b4fa90b704580e409338c1b8ce30848b85842c
Leonardo Sarra <leonardosarra@outlook.com>
2018-07-07 20:05:54 +0200

    User: Add management page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get '/book_api', to: 'api#get_info'
+  get '/manage_users', to: 'management#manage_users'
+  put '/manage_users/promote', to: 'management#promote', as: 'promote'
+  put '/manage_users/demote', to: 'management#demote', as: 'demote'
+  post '/manage_users/change_role', to: 'management#change_role', as:'change_role'
   devise_scope :user do
     get '/users/login_email', to: 'login#login_email'
   end
