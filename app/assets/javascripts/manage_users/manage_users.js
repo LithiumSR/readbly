061128ff91da45745e25863223b892a10d6c111b
Leonardo Sarra <leonardosarra@outlook.com>
2018-07-07 20:06:47 +0200

    User: Improve logic of management page using JavaScript

---
+++ app/assets/javascripts/manage_users/manage_users.js
@@ -0,0 +1,26 @@
+function load(){
+    let names = document.getElementsByClassName("name");
+    let change_buttons = document.getElementsByClassName("change");
+    let select = document.getElementsByClassName("select");
+    let option = document.createElement("option");
+    option.text = "-";
+    for(var x=0; x < names.length; x++)
+    {
+        if(names[x].innerHTML === '') names[x].innerHTML="OAuth";
+        else names[x].innerHTML="Email";
+        change_buttons[x].disabled = true;
+        let tmp = select[x].options[0];
+        select[x].options[select[x].options.length] = new Option('-', '-');
+        select[x].options[0] = select[x].options[select[x].options.length-1];
+        select[x].options[select[x].options.length] = tmp;
+        select[x].selectedIndex = 0;
+        select[x].addEventListener("change", function() {
+            let select = document.getElementsByClassName("select");
+            let change_buttons = document.getElementsByClassName("change");
+            let x;
+            for(x=0; x < select.length; x++) if(select[x]==this) break;
+            if (this.value == "-") change_buttons[x].disabled = true;
+            else change_buttons[x].disabled=false;
+        });
+    }
+}
