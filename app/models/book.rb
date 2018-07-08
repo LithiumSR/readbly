3c3a2262128c62b6d2f8aecbbf89e7020517d45e
Leonardo Sarra <leonardosarra@outlook.com>
2018-07-08 16:15:01 +0200

    Book: Improve model validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,11 @@ class Book < ApplicationRecord
   validates :overview, presence: true
   validates :isbn, presence: true
   validates :released_at, :numericality => { :greater_than_or_equal_to => 0 }, presence: true
+  validate :check_length
+  def check_length
+    unless isbn.gsub(" ","").size == 10 or isbn.gsub(" ","").size == 13
+      errors.add(:Isbn, "length must be 10 or 13")
+    end
+  end
+
 end
