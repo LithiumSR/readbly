dfffbea966a4d462d4cc487623e8471fce0ae3bb
Leonardo Sarra <leonardosarra@outlook.com>
2018-07-09 14:43:13 +0200

    Reservation: Add after_create for model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,11 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :book
+  after_create :assign_default_values
+
+  def assign_default_values
+    self.isReturned = false
+    self.isLoan = false
+    self.isPostponed = false
+  end
 end
