dd0b91046f820baaa3c46b709a8db49185210508
Leonardo Sarra <leonardosarra@outlook.com>
2018-07-09 20:18:59 +0200

    Reservations: Improve CSS and JS code for management panel

--- app/controllers/management_reservations_controller.rb
+++ app/controllers/management_reservations_controller.rb
@@ -1,9 +1,16 @@
 class ManagementReservationsController < ApplicationController
   before_action :canManage
   def manage_reservations
-    @active_reservations = Reservation.all.select{|i| i.isReturned == false}.select{|i| i.isLoan == true}.paginate(page: params[:page], per_page: 15)
-    @pending_reservations = Reservation.all.select{|i| i.isReturned == false}.select{|i| i.isLoan == false}.paginate(page: params[:page], per_page: 15)
-    @completed_reservations = Reservation.all.select{|i| i.isReturned == true}.select{|i| i.isLoan == true}.paginate(page: params[:page], per_page: 15)
+    @active_reservations = Reservation.all.select{|i| !i.isReturned}.select{|i| i.isLoan}.paginate(page: params[:page], per_page: 15)
+    @pending_reservations = Reservation.all.select{|i| !i.isReturned}.select{|i| !i.isLoan}.paginate(page: params[:page], per_page: 15)
+    @completed_reservations = Reservation.all.select{|i| i.isReturned}.select{|i| i.isLoan}.paginate(page: params[:page], per_page: 15)
+  end
+
+
+  def my_reservations
+    @active_reservations = Reservation.all.select{|i| !i.isReturned}.select{|i| i.isLoan}.select{|i| i.user_id = current_user.id}.paginate(page: params[:page], per_page: 15)
+    @pending_reservations = Reservation.all.select{|i| !i.isReturned}.select{|i| !i.isLoan}.select{|i| i.user_id = current_user.id}.paginate(page: params[:page], per_page: 15)
+    @completed_reservations = Reservation.all.select{|i| i.isReturned}.select{|i| i.isLoan}.select{|i| i.user_id = current_user.id}.paginate(page: params[:page], per_page: 15)
   end
 
   def canManage
